f318f28b9496c9ebab61e189b72a3e6795604a52
satyendra parihar <pariharsatyendra89@gmail.com>
2019-07-09 20:11:25 +0530

    remove byebug

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ end
 mother_array = User.where id: (1..10)
 father_array = User.where id: (11..20)
 children_array = User.where id: (21..100)
-byebug
+
 children_array.each do |user|
   user.update(father_id: father_array.pluck(:id).sample, mother_id: mother_array.pluck(:id).sample)
 end
